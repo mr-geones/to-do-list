756bb4b12b0398db008321ba21ed9a2863a808b4
Geones <mr.geones@yahoo.com>
2023-03-07 12:12:25 +0100

    created a simple to-do list

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+function addItem() {
+  const list = document.querySelector('#todo');
+  const item = document.createElement('li');
+  const content = prompt('Enter description');
 
+  if (content !== '') {
+    list.appendChild(item);
+    item.innerHTML = content;
+    console.log(content);
+  }
+  else {
+    alert('Missing description, no item has been created.');
+  }
+}
+
+function changeItem() {
+  const list = document.querySelectorAll('li');
+  const lastItem = list.length - 1;
+  const content = prompt('Change description');
+  
+  list[lastItem].innerHTML = content;
+}
+
+function removeItem() {
+  const list = document.querySelector('#todo');
+  list.removeChild(list.lastElementChild);
+}
